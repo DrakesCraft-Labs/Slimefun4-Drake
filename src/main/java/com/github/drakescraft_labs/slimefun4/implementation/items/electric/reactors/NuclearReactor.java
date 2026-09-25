1b504c976f6666e41c6ff28b0655b68089db0f9e
Pablo Elías Avendaño Miranda <pablo.elias.miranda.292003@gmail.com>
2026-09-24 21:22:16 -0300

    feat(nuclear): add active gamma radiation hazard to NuclearReactor

--- src/main/java/com/github/drakescraft_labs/slimefun4/implementation/items/electric/reactors/NuclearReactor.java
+++ src/main/java/com/github/drakescraft_labs/slimefun4/implementation/items/electric/reactors/NuclearReactor.java
@@ -1,25 +1,39 @@
 package com.github.drakescraft_labs.slimefun4.implementation.items.electric.reactors;
 
+import java.util.Optional;
+
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 
+import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import com.github.drakescraft_labs.slimefun4.api.items.ItemGroup;
 import com.github.drakescraft_labs.slimefun4.api.items.SlimefunItemStack;
+import com.github.drakescraft_labs.slimefun4.api.player.PlayerProfile;
 import com.github.drakescraft_labs.slimefun4.api.recipes.RecipeType;
+import com.github.drakescraft_labs.slimefun4.core.attributes.ProtectionType;
 import com.github.drakescraft_labs.slimefun4.core.attributes.Radioactive;
+import com.github.drakescraft_labs.slimefun4.implementation.Slimefun;
 import com.github.drakescraft_labs.slimefun4.implementation.SlimefunItems;
 
 import com.github.drakescraft_labs.slimefun4.legacy.Objects.SlimefunItem.abstractItems.MachineFuel;
+import com.github.drakescraft_labs.slimefun4.utils.RadiationUtils;
 
 /**
  * The {@link NuclearReactor} is an implementation of {@link Reactor} that uses
  * any {@link Radioactive} material to generate energy.
- * It needs water coolant as well as a steady supply of Reactor Coolant Cells
+ * It needs water coolant as well as a steady supply of Reactor Coolant Cells.
+ * While actively running, it emits hazardous gamma radiation to any player within
+ * 7 blocks unless protected by a full hazmat suit.
  * 
  * @author TheBusyBiscuit
+ * @author DrakesCraft-Labs
  * 
  * @see NetherStarReactor
  *
@@ -55,8 +69,25 @@ public abstract class NuclearReactor extends Reactor {
 
     @Override
     public void extraTick(@Nonnull Location l) {
-        // This machine does not need to perform anything while ticking
-        // The Nether Star Reactor uses this method to generate the Wither Effect
+        Slimefun.runSync(() -> {
+            for (Entity entity : l.getWorld().getNearbyEntities(l, 7, 7, 7, n -> n instanceof Player && n.isValid())) {
+                if (entity instanceof Player player) {
+                    if (player.getGameMode() == GameMode.CREATIVE || player.getGameMode() == GameMode.SPECTATOR) {
+                        continue;
+                    }
+                    Optional<PlayerProfile> profile = PlayerProfile.find(player);
+                    if (profile.isPresent() && !profile.get().hasFullProtectionAgainst(ProtectionType.RADIATION)) {
+                        int exposure = RadiationUtils.getExposure(player);
+                        if (exposure == 0) {
+                            Slimefun.getLocalization().sendMessage(player, "messages.radiation");
+                        }
+                        RadiationUtils.addExposure(player, 2);
+                        player.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 60, 0));
+                        player.addPotionEffect(new PotionEffect(PotionEffectType.NAUSEA, 100, 0));
+                    }
+                }
+            }
+        });
     }
 
 }
